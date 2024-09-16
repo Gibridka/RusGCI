5625711915c658a6eb3689e52dfc014a0946dd6d
Gibridka <lololoshka.vowa@yandex.ru>
2024-09-16 20:08:35 +0700

    Update challenges.js

--- js/features/challenges.js
+++ js/features/challenges.js
@@ -232,10 +232,10 @@ el.update.chal = ()=>{
 
                     tmp.el["chal_comp_"+i].setTxt(format(l,0) + " / " + format(c.max,0))
                     tmp.el["chal_eff_"+i].setHTML(c.effDesc(tmp.chal.eff[i]))
-                    tmp.el["chal_pro_"+i].setTxt(completed ? "Completed" : inChal(i) ? "Progress" : "Inactive")
+                    tmp.el["chal_pro_"+i].setTxt(completed ? "Выполнено" : inChal(i) ? "Прогресс" : "Неактивен")
                     tmp.el["chal_pro_"+i].setClasses({[completed ? "green" : inChal(i) ? "yellow" : "red"]: true})
 
-                    tmp.el["chal_goal_"+i].setTxt("Goal: "+c.goalDesc(tmp.chal.goal[i]))
+                    tmp.el["chal_goal_"+i].setTxt("Цель: "+c.goalDesc(tmp.chal.goal[i]))
                     tmp.el["chal_goal_"+i].setClasses({[inChal(i) && a >= tmp.chal.goal[i] ? "green" : "red"]: true})
                 }
             }
